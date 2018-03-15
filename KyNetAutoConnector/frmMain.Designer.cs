0a71d3cf8ddc01e29544d985c01f569dc7421cc5
ningboliuwei <liuwei@nbu.edu.cn>
2018-03-15 18:05:08 +0800

    InitialCommit

---
+++ KyNetAutoConnector/frmMain.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace KyNetAutoConnector
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.chkRunWhenStartup = new System.Windows.Forms.CheckBox();
+            this.updnReconnect = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.updnReconnect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.txtUrl, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtUsername, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtPassword, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnRun, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(356, 165);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrl.Location = new System.Drawing.Point(83, 3);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(270, 21);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.Text = "http://10.22.115.123";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsername.Location = new System.Drawing.Point(83, 33);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(270, 21);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.Text = "0428204";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Location = new System.Drawing.Point(83, 63);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(270, 21);
+            this.txtPassword.TabIndex = 0;
+            this.txtPassword.Text = "291019";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "网址";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "用户名";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "密码";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRun
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.btnRun, 2);
+            this.btnRun.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnRun.Location = new System.Drawing.Point(3, 123);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(350, 39);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "开始(&R)";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 2);
+            this.flowLayoutPanel1.Controls.Add(this.chkRunWhenStartup);
+            this.flowLayoutPanel1.Controls.Add(this.label4);
+            this.flowLayoutPanel1.Controls.Add(this.updnReconnect);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 93);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(350, 24);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // chkRunWhenStartup
+            // 
+            this.chkRunWhenStartup.AutoSize = true;
+            this.chkRunWhenStartup.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chkRunWhenStartup.Location = new System.Drawing.Point(3, 3);
+            this.chkRunWhenStartup.Name = "chkRunWhenStartup";
+            this.chkRunWhenStartup.Size = new System.Drawing.Size(132, 21);
+            this.chkRunWhenStartup.TabIndex = 0;
+            this.chkRunWhenStartup.Text = "系统启动时自动运行";
+            this.chkRunWhenStartup.UseVisualStyleBackColor = true;
+            // 
+            // updnReconnect
+            // 
+            this.updnReconnect.Location = new System.Drawing.Point(272, 3);
+            this.updnReconnect.Name = "updnReconnect";
+            this.updnReconnect.Size = new System.Drawing.Size(64, 21);
+            this.updnReconnect.TabIndex = 1;
+            this.updnReconnect.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Location = new System.Drawing.Point(141, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 27);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "自动重连间隔（分钟）";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 165);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frmMain";
+            this.Text = "科院外网自动连接器";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmMain_FormClosed);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.updnReconnect)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.CheckBox chkRunWhenStartup;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown updnReconnect;
+    }
+}
+
---
+++ KyNetAutoConnector/frmMain.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace KyNetAutoConnector
+{
+    public partial class frmMain : Form
+    {
+        private InputSimulator _inputSimulator = new InputSimulator();
+        private string _configPath = Path.Combine(Application.ExecutablePath, "config.xml");
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (IsOffline())
+            {
+                AutoLogin();
+            }
+        }
+
+        public void AutoLogin()
+        {
+            var url = txtUrl.Text.Trim();
+
+            System.Diagnostics.Process.Start(url);
+            Thread.Sleep(3000);
+            InputTab();
+            InputData(txtUsername.Text.Trim());
+            InputTab();
+            InputData(txtPassword.Text.Trim());
+            InputEnter();
+            Thread.Sleep(3000);
+            CloseBrowser();
+        }
+
+        public void InputData(string data)
+        {
+            _inputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_A); //全选当前输入框
+            _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.DELETE);
+            _inputSimulator.Keyboard.TextEntry(data);
+            _inputSimulator.Keyboard.Sleep(50);
+        }
+
+        public void InputTab()
+        {
+            _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.TAB);
+            _inputSimulator.Keyboard.Sleep(50);
+        }
+
+        public void InputEnter()
+        {
+            _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+            _inputSimulator.Keyboard.Sleep(50);
+        }
+
+        public void CloseBrowser()
+        {
+            _inputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_W);
+            _inputSimulator.Keyboard.Sleep(50);
+        }
+
+        public bool IsOffline()
+        {
+            var ping = new Ping();
+            var reply = ping.Send("www.baidu.com", 1000);//百度IP
+            if (reply != null && reply.Status == IPStatus.Success)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void LoadSettings()
+        {
+
+        }
+
+        public void SaveSettigns()
+        {
+            var settings = new Settings()
+            {
+                Url = txtUrl.Text.Trim(),
+                Username = txtUsername.Text.Trim(),
+                Password = txtPassword.Text.Trim(),
+                RunWhenStartup = chkRunWhenStartup.Checked,
+                AutoReconnectInterval = Convert.ToInt32(updnReconnect.Value)
+            };
+
+            var serializer = new XmlSerializer(typeof(Settings));
+            var writer = new StreamWriter(_configPath);
+            serializer.Serialize(writer, settings);
+        }
+
+        [Serializable]
+        public class Settings
+        {
+            public string Url { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public bool RunWhenStartup { get; set; }
+            public int AutoReconnectInterval { get; set; }
+        }
+
+        private void frmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SaveSettigns();
+        }
+    }
+}
